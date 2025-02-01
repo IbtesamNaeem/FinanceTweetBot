2bc30f6da3854de38aed9d8d1c1d793c8e1028b6
Ibtesam Naeem <ibtesamnaeem@gmail.com>
2025-02-01 18:57:15 -0500

    - Created logging

---
+++ config/logger.py
@@ -0,0 +1,16 @@
+import logging
+
+def setup_logger(name=None, level=logging.INFO):
+    """
+    Sets up a reusable logger with consistent formatting.
+
+    :param name: Optional logger name (default: root logger)
+    :param level: Logging level (default: INFO)
+    :return: Configured logger instance
+    """
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        level=level,
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    return logging.getLogger(name)
