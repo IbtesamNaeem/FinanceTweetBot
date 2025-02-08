1f2fdd4de769a66af371736f83c4e66a357afc65
Ibtesam Naeem <ibtesamnaeem@gmail.com>
2025-02-07 21:23:06 -0500

    - Removed BBBY

---
+++ scraping/stock_categories.py
@@ -0,0 +1,11 @@
+crypto_holdings = ["MSTR", "BITO"]
+crypto_mining = ["RIOT", "MARA", "HUT"]
+blockchain_services = ["COIN", "HOOD"]
+meme_stocks = ["GME", "AMC",]
+
+stock_categories = {
+    "Crypto": crypto_holdings,
+    "Crypto Mining": crypto_mining,
+    "Blockchain Services": blockchain_services,
+    "Meme Stocks": meme_stocks,
+}
