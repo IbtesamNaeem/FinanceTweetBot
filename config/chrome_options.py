4092d313d6d6a975ad5bdd7d7ff822f7ccdc9a3a
Ibtesam Naeem <ibtesamnaeem@gmail.com>
2025-02-10 10:09:58 -0500

    - Refactor headless mode

--- config/chrome_options.py
+++ config/chrome_options.py
@@ -10,7 +10,7 @@ def chrome_options():
     """
     chrome_options = Options()
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--headless=new")
+    # chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--window-size=1920x1080")
